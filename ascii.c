72952e22c97c9c8bcc187e3c7e55bb0ee5dfc4ae
Yakshita <101899906+Yakshita1802@users.noreply.github.com>
2022-09-30 22:06:12 -0400

    Update ascii.c

--- ascii.c
+++ ascii.c
@@ -4,7 +4,7 @@ int main() {
 	//TODO: Please replace ??? in line 7 with an integer(not a char) so that your code builds and prints out ch = * 
 	//Please only change ???
 	
-	int _ch = 65; // 65 correspond to A in the ASCII value
+	int _ch = 42; // 42 correspond to * in the ASCII value
 	printf("ch = %c\n", _ch);	
 	return 0;
 }
